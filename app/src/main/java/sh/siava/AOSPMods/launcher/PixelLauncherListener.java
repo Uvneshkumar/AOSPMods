47aea1db30e1b1ffa8bd9466e0e74c8d30ec19ba
Uvnesh <uvneshkumar@gmail.com>
2023-05-17 14:04:50 +0530

    enablePixelVibration

--- app/src/main/java/sh/siava/AOSPMods/launcher/PixelLauncherListener.java
+++ app/src/main/java/sh/siava/AOSPMods/launcher/PixelLauncherListener.java
@@ -1,13 +1,25 @@
 package sh.siava.AOSPMods.launcher;
 
-import android.content.Context;
+import static de.robv.android.xposed.XposedHelpers.findClassIfExists;
+import static sh.siava.AOSPMods.utils.Helpers.tryHookAllMethods;
 
+import android.content.Context;
+import android.os.VibrationAttributes;
+import android.os.VibrationEffect;
+import android.view.View;
+
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import sh.siava.AOSPMods.AOSPMods;
+import sh.siava.AOSPMods.XPrefs;
 import sh.siava.AOSPMods.XposedModPack;
+import sh.siava.AOSPMods.utils.SystemUtils;
 
 @SuppressWarnings("RedundantThrows")
 public class PixelLauncherListener extends XposedModPack {
+
+	final boolean[] hasVibrated = {false};
+
 	private static final String listenPackage = AOSPMods.LAUNCHER_PACKAGE;
 
 	public PixelLauncherListener(Context context) {
@@ -25,6 +37,23 @@ public class PixelLauncherListener extends XposedModPack {
 
 	@Override
 	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-
+		if (XPrefs.Xprefs.getBoolean("enablePixelVibration", false)) {
+			Class<?> AllAppsRecyclerView = findClassIfExists("com.android.launcher3.allapps.AllAppsRecyclerView", lpparam.classLoader);
+			if (AllAppsRecyclerView != null) {
+				tryHookAllMethods(AllAppsRecyclerView, "onUpdateScrollbar", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						if (!((View) (param.thisObject)).canScrollVertically(1) || !((View) (param.thisObject)).canScrollVertically(-1)) {
+							if (!hasVibrated[0]) {
+								SystemUtils.vibrate(VibrationEffect.EFFECT_TICK, VibrationAttributes.USAGE_ACCESSIBILITY);
+								hasVibrated[0] = true;
+							}
+						} else {
+							hasVibrated[0] = false;
+						}
+					}
+				});
+			}
+		}
 	}
 }
