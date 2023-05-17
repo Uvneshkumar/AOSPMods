5afaf54fa1e74de710d9c071b195510c6578f606
Uvnesh <uvneshkumar@gmail.com>
2023-05-17 14:22:05 +0530

    disableLockScreenBounce

--- app/src/main/java/sh/siava/AOSPMods/systemui/SystemUIListener.java
+++ app/src/main/java/sh/siava/AOSPMods/systemui/SystemUIListener.java
@@ -5,6 +5,7 @@ import static sh.siava.AOSPMods.XPrefs.Xprefs;
 import static sh.siava.AOSPMods.utils.Helpers.tryHookAllMethods;
 
 import android.content.Context;
+import android.view.MotionEvent;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
@@ -26,7 +27,6 @@ public class SystemUIListener extends XposedModPack {
 	@Override
 	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
 		if (!lpparam.packageName.equals(listenPackage)) return;
-
 		if (Xprefs.getBoolean("disableLocationPrivacyIndicator", false)) {
 			Class<?> PrivacyConfig = findClassIfExists("com.android.systemui.privacy.PrivacyConfig", lpparam.classLoader);
 			if (PrivacyConfig != null) {
@@ -34,6 +34,19 @@ public class SystemUIListener extends XposedModPack {
 					@Override
 					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 						param.setResult(false);
+					}
+				});
+			}
+		}
+		if (Xprefs.getBoolean("disableLockScreenBounce", false)) {
+			Class<?> DragDownHelper = findClassIfExists("com.android.systemui.statusbar.DragDownHelper", lpparam.classLoader);
+			if (DragDownHelper != null) {
+				tryHookAllMethods(DragDownHelper, "onInterceptTouchEvent", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						if (((MotionEvent) (param.args[0])).getActionMasked() == MotionEvent.ACTION_UP) {
+							param.setResult(true);
+						}
 					}
 				});
 			}
