a8e65d32a8702c3601d0aafe4b5f0c997429b4ae
Uvnesh <uvneshkumar@gmail.com>
2023-05-31 14:38:23 +0530

    Disable (Pixel) Launcher Top Shadow

--- app/src/main/java/sh/siava/AOSPMods/myListeners/AllAppsListener.java
+++ app/src/main/java/sh/siava/AOSPMods/myListeners/AllAppsListener.java
@@ -1,7 +1,11 @@
 package sh.siava.AOSPMods.myListeners;
 
+import static de.robv.android.xposed.XposedHelpers.findClassIfExists;
+import static sh.siava.AOSPMods.utils.Helpers.tryHookAllMethods;
+
 import android.content.Context;
 
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import sh.siava.AOSPMods.XposedModPack;
 
@@ -24,6 +28,17 @@ public class AllAppsListener extends XposedModPack {
 
 	@Override
 	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-
+		// Disable (Pixel) Launcher Top Shadow
+		Class<?> ThemesClass = findClassIfExists("com.android.launcher3.util.Themes", lpparam.classLoader);
+		if (ThemesClass != null) {
+			tryHookAllMethods(ThemesClass, "getAttrDrawable", new XC_MethodHook() {
+				@Override
+				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+					if ((int) param.args[1] != 16843534) {
+						param.setResult(null);
+					}
+				}
+			});
+		}
 	}
 }
