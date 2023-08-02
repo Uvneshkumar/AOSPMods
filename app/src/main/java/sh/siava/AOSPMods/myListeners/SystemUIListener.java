43455a8cd01b87d1f1a23ad8a9f0251b6971ef12
Uvnesh <uvneshkumar@gmail.com>
2023-08-02 21:14:43 +0530

    dt2sStatusBar

--- app/src/main/java/sh/siava/AOSPMods/myListeners/SystemUIListener.java
+++ app/src/main/java/sh/siava/AOSPMods/myListeners/SystemUIListener.java
@@ -13,6 +13,8 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.VibrationAttributes;
 import android.os.VibrationEffect;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -116,6 +118,10 @@ public class SystemUIListener extends XposedModPack {
 
 	public final String CLIPBOARD_OVERLAY_SHOW_ACTIONS = "clipboard_overlay_show_actions";
 	public final String NAMESPACE_SYSTEMUI = "systemui";
+
+	private static final int SHADE = 0; // frameworks/base/packages/SystemUI/src/com/android/systemui/statusbar/StatusBarState.java - screen unlocked - pulsing means screen is locked - shade locked means (Q)QS is open on lockscreen
+	GestureDetector mLockscreenDoubleTapToSleep; // event callback for double tap to sleep detection of statusbar only
+	private Object NotificationPanelViewController;
 
 	@Override
 	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
@@ -297,6 +303,35 @@ public class SystemUIListener extends XposedModPack {
 			com.topjohnwu.superuser.Shell.cmd("pm " + mode + " com.android.systemui/.tuner.TunerActivity").exec();
 		} catch (Exception ignored) {
 		}
+		if (Xprefs.getBoolean("dt2sStatusBar", false)) {
+			mLockscreenDoubleTapToSleep = new GestureDetector(mContext, new GestureDetector.SimpleOnGestureListener() {
+				@Override
+				public boolean onDoubleTap(MotionEvent e) {
+					SystemUtils.Sleep();
+					return true;
+				}
+			});
+			Class<?> NotificationPanelViewControllerClass = findClassIfExists("com.android.systemui.shade.NotificationPanelViewController", lpparam.classLoader);
+			if (NotificationPanelViewControllerClass != null) {
+				tryHookAllConstructors(NotificationPanelViewControllerClass, new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						NotificationPanelViewController = param.thisObject;
+						try {
+							hookTouchHandler(getObjectField(param.thisObject, "mStatusBarViewTouchEventHandler").getClass());
+						} catch (Throwable ignored) {
+						}
+					}
+				});
+				tryHookAllMethods(NotificationPanelViewControllerClass, "createTouchHandler", new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						NotificationPanelViewController = param.thisObject;
+						hookTouchHandler(param.getResult().getClass());
+					}
+				});
+			}
+		}
 	}
 
 	private void setQSFooterText() {
@@ -313,6 +348,24 @@ public class SystemUIListener extends XposedModPack {
 			}
 		} catch (Throwable ignored) {
 		} //probably not initiated yet
+	}
+
+	private void hookTouchHandler(Class<?> TouchHanlderClass) {
+		XC_MethodHook touchHook = new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				if (!(Xprefs.getBoolean("dt2sStatusBar", false))) return;
+				// double tap to sleep, statusbar only
+				if (!(boolean) getObjectField(NotificationPanelViewController, "mPulsing")
+						&& !(boolean) getObjectField(NotificationPanelViewController, "mDozing")
+						&& (int) getObjectField(NotificationPanelViewController, "mBarState") == SHADE
+						&& (boolean) callMethod(NotificationPanelViewController, "isFullyCollapsed")) {
+					mLockscreenDoubleTapToSleep.onTouchEvent((MotionEvent) param.args[param.args.length - 1]);
+				}
+			}
+		};
+		tryHookAllMethods(TouchHanlderClass, "onTouch", touchHook); // 13 QPR2
+		tryHookAllMethods(TouchHanlderClass, "handleTouchEvent", touchHook); // A13 R18
 	}
 
 	@Override
