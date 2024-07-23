32856df0ded032f33139f419277b38cefbed3b48
Uvnesh <uvneshkumar@gmail.com>
2024-07-23 20:11:22 +0530

    Additional Trust

--- app/src/main/java/sh/siava/AOSPMods/myListeners/AllAppsListener.java
+++ app/src/main/java/sh/siava/AOSPMods/myListeners/AllAppsListener.java
@@ -1,5 +1,6 @@
 package sh.siava.AOSPMods.myListeners;
 
+import static de.robv.android.xposed.XposedHelpers.callMethod;
 import static de.robv.android.xposed.XposedHelpers.findClassIfExists;
 import static de.robv.android.xposed.XposedHelpers.setIntField;
 import static sh.siava.AOSPMods.utils.Helpers.tryHookAllConstructors;
@@ -130,6 +131,110 @@ public class AllAppsListener extends XposedModPack {
 					@Override
 					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 						param.setResult(STRONG_AUTH_NOT_REQUIRED);
+					}
+				});
+			}
+
+			Class<?> TrustAgentService = findClassIfExists("android.service.trust.TrustAgentService", lpparam.classLoader);
+			if (TrustAgentService != null) {
+				tryHookAllMethods(TrustAgentService, "onTrustTimeout", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						try {
+							callMethod(param.thisObject, "grantTrust", "Kept unlocked by Extend Unlock", 0, 8);
+						} catch (Exception ignored) {
+						}
+						param.setResult(null);
+					}
+				});
+				tryHookAllMethods(TrustAgentService, "grantTrust", new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {}
+				});
+				tryHookAllMethods(TrustAgentService, "revokeTrust", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						param.setResult(null);
+					}
+				});
+				tryHookAllMethods(TrustAgentService, "lockUser", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						param.setResult(null);
+					}
+				});
+			}
+
+			Class<?> TrustAgentWrapper = findClassIfExists("com.android.server.trust.TrustAgentWrapper", lpparam.classLoader);
+			if (TrustAgentWrapper != null) {
+				tryHookAllMethods(TrustAgentWrapper, "onTrustTimeout", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						try {
+							callMethod(param.thisObject, "grantTrust", "Kept unlocked by Extend Unlock", 0, 8);
+						} catch (Exception ignored) {
+						}
+						param.setResult(null);
+					}
+				});
+			}
+
+			Class<?> GoogleTrustAgentChimeraService = findClassIfExists("com.google.android.gms.trustagent.GoogleTrustAgentChimeraService", lpparam.classLoader);
+			if (GoogleTrustAgentChimeraService != null) {
+				tryHookAllMethods(GoogleTrustAgentChimeraService, "onTrustTimeout", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						try {
+							callMethod(param.thisObject, "grantTrust", "Kept unlocked by Extend Unlock", 0, 8);
+						} catch (Exception ignored) {
+						}
+						param.setResult(null);
+					}
+				});
+				tryHookAllMethods(GoogleTrustAgentChimeraService, "grantTrust", new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {}
+				});
+				tryHookAllMethods(GoogleTrustAgentChimeraService, "revokeTrust", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						param.setResult(null);
+					}
+				});
+				tryHookAllMethods(GoogleTrustAgentChimeraService, "lockUser", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						param.setResult(null);
+					}
+				});
+			}
+
+			Class<?> GoogleChimeraTrustAgentService = findClassIfExists("com.google.android.chimera.TrustAgentService", lpparam.classLoader);
+			if (GoogleChimeraTrustAgentService != null) {
+				tryHookAllMethods(GoogleChimeraTrustAgentService, "onTrustTimeout", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						try {
+							callMethod(param.thisObject, "grantTrust", "Kept unlocked by Extend Unlock", 0, 8);
+						} catch (Exception ignored) {
+						}
+						param.setResult(null);
+					}
+				});
+				tryHookAllMethods(GoogleChimeraTrustAgentService, "grantTrust", new XC_MethodHook() {
+					@Override
+					protected void afterHookedMethod(MethodHookParam param) throws Throwable {}
+				});
+				tryHookAllMethods(GoogleChimeraTrustAgentService, "revokeTrust", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						param.setResult(null);
+					}
+				});
+				tryHookAllMethods(GoogleChimeraTrustAgentService, "lockUser", new XC_MethodHook() {
+					@Override
+					protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						param.setResult(null);
 					}
 				});
 			}
