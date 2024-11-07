39e7065b2bfb5dec1a3f66a50ecab634fafef4b7
Uvnesh <uvnesh.k@tringapps.com>
2024-11-07 16:28:58 +0530

    AOD animDuration = 400 (Similar to A15)

--- app/src/main/java/sh/siava/AOSPMods/myListeners/PixelLauncherListener.java
+++ app/src/main/java/sh/siava/AOSPMods/myListeners/PixelLauncherListener.java
@@ -130,7 +130,7 @@ public class PixelLauncherListener extends XposedModPack {
 							backgroundView.setY(-frameHeight);
 							int pixelsToAdjust = 1; // Additional Pixels to avoid slight gap
 							blackView.setY((-(frameHeight * 2)) + pixelsToAdjust);
-							long animDuration = 350;
+							long animDuration = 400;
 							ObjectAnimator bgFirst = ObjectAnimator.ofFloat(backgroundView, "y", backgroundView.getY(), frameHeight - pixelsToAdjust);
 							bgFirst.setDuration(animDuration);
 							ObjectAnimator blackFirst = ObjectAnimator.ofFloat(blackView, "y", blackView.getY(), 0);
