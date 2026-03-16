1629818842555f259c0e8fdde58711586a2cdead
Uvnesh <uvneshkumar@gmail.com>
2026-03-16 06:56:19 +0530

    add Do Not Disturb indicator and refactor CustomDateAlarmLayout to a horizontal structure

--- app/src/main/java/sh/siava/AOSPMods/myListeners/helper/CustomDateAlarmLayout.kt
+++ app/src/main/java/sh/siava/AOSPMods/myListeners/helper/CustomDateAlarmLayout.kt
@@ -1,7 +1,12 @@
 package sh.siava.AOSPMods.myListeners.helper
 
 import android.app.AlarmManager
+import android.app.NotificationManager
+import android.app.NotificationManager.ACTION_INTERRUPTION_FILTER_CHANGED
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.Color
 import android.os.Handler
 import android.os.Looper
@@ -11,6 +16,7 @@ import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.isVisible
 import sh.siava.AOSPMods.R
 import sh.siava.AOSPMods.XPrefs
 import java.text.SimpleDateFormat
@@ -20,12 +26,14 @@ import java.util.Locale
 
 class CustomDateAlarmLayout(context: Context) : LinearLayout(context) {
 
+    val notificationManager =
+        context.getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager
     private val dateTextView: TextView
     private val alarmTextView: TextView
     private val alarmIcon: ImageView
-    private val alarmLayout: LinearLayout
-    private val alarmManager: AlarmManager? =
-        context.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
+    private val dndIcon: ImageView
+    private val firstLine: LinearLayout
+    private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
     private val handler = Handler(Looper.getMainLooper())
     private val timeRunnable: Runnable
     private val dateFormat = SimpleDateFormat("EEE, d MMM", Locale.getDefault())
@@ -40,6 +48,22 @@ class CustomDateAlarmLayout(context: Context) : LinearLayout(context) {
     init {
         orientation = VERTICAL
         setPadding(0, dpToPx(4), 0, dpToPx(4))
+        firstLine = LinearLayout(context).apply {
+            gravity = Gravity.CENTER_VERTICAL
+            orientation = HORIZONTAL
+        }
+        dndIcon = ImageView(context).apply {
+            setImageDrawable(
+                ResourcesCompat.getDrawable(
+                    XPrefs.modRes, R.drawable.ic_do_not_disturb_on, null
+                )
+            )
+            visibility = GONE
+        }
+        val dndParams = LayoutParams(dpToPx(24), dpToPx(24)).apply {
+            rightMargin = dpToPx(8)
+        }
+        firstLine.addView(dndIcon, dndParams)
         dateTextView = TextView(context).apply {
             setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
             setTextColor(Color.WHITE)
@@ -47,12 +71,7 @@ class CustomDateAlarmLayout(context: Context) : LinearLayout(context) {
             fontVariationSettings = boldTextVariation
             includeFontPadding = false
         }
-        addView(dateTextView)
-        alarmLayout = LinearLayout(context).apply {
-            gravity = Gravity.CENTER_VERTICAL
-            orientation = HORIZONTAL
-            visibility = GONE
-        }
+        firstLine.addView(dateTextView)
         alarmIcon = ImageView(context).apply {
             setImageDrawable(
                 ResourcesCompat.getDrawable(
@@ -61,9 +80,9 @@ class CustomDateAlarmLayout(context: Context) : LinearLayout(context) {
             )
         }
         val iconParams = LayoutParams(dpToPx(20), dpToPx(20)).apply {
-            rightMargin = dpToPx(8)
+            leftMargin = dpToPx(8)
         }
-        alarmLayout.addView(alarmIcon, iconParams)
+        firstLine.addView(alarmIcon, iconParams)
         alarmTextView = TextView(context).apply {
             setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
             setTextColor(Color.WHITE)
@@ -71,11 +90,15 @@ class CustomDateAlarmLayout(context: Context) : LinearLayout(context) {
             fontVariationSettings = boldTextVariation
             includeFontPadding = false
         }
-        alarmLayout.addView(alarmTextView)
-        val alarmParams = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
-            topMargin = dpToPx(4)
-        }
-        addView(alarmLayout, alarmParams)
+        val alarmTextParams =
+            LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
+                leftMargin = dpToPx(8)
+            }
+        firstLine.addView(alarmTextView, alarmTextParams)
+        addView(firstLine)
+        updateSilentState()
+        val filter = IntentFilter(ACTION_INTERRUPTION_FILTER_CHANGED)
+        context.registerReceiver(DoNotDisturbChangeReceiver(), filter)
         timeRunnable = object : Runnable {
             override fun run() {
                 showCurrentDate()
@@ -116,9 +139,24 @@ class CustomDateAlarmLayout(context: Context) : LinearLayout(context) {
         }
         if (alarmVisibility != lastShownAlarmVisibility) {
             lastShownAlarmVisibility = alarmVisibility
-            alarmLayout.visibility = alarmVisibility
+            alarmIcon.visibility = alarmVisibility
+            alarmTextView.visibility = alarmVisibility
         }
     }
 
+    fun isDndEnabled(): Boolean {
+        return notificationManager?.currentInterruptionFilter != NotificationManager.INTERRUPTION_FILTER_ALL
+    }
+
+    fun updateSilentState() {
+        dndIcon.isVisible = isDndEnabled()
+    }
+
     private fun dpToPx(dp: Int): Int = (dp * context.resources.displayMetrics.density).toInt()
+
+    private inner class DoNotDisturbChangeReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            updateSilentState()
+        }
+    }
 }
